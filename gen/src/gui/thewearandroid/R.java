79a2dff8c2f4bbad50c16a5c78f986fb8c735f0f
Corneel <corneel.decraene@gmail.com>
2013-10-10 15:15:49 +0200

    Delete R.java
    Deleted file because it's automatically generated by the IDE, to prevent merge conflicts

--- gen/src/gui/thewearandroid/R.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package src.gui.thewearandroid;
-
-/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
-public final class R {
-}

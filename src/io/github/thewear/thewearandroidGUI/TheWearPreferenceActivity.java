5871335442ed4bea03d554c6f26e9c7d3a09b639
Corneel-D <Corneel-D@TheWear.nogwat>
2014-02-13 20:29:57 +0100

    Added "Up" navigation to the preferences activity and added necessary libraries to our project

--- src/io/github/thewear/thewearandroidGUI/TheWearPreferenceActivity.java
+++ src/io/github/thewear/thewearandroidGUI/TheWearPreferenceActivity.java
@@ -1,8 +1,12 @@
 package io.github.thewear.thewearandroidGUI;
 
 import io.github.thewear.thewearandroid.R;
+import android.annotation.TargetApi;
+import android.app.ActionBar;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.util.Log;
 
 public class TheWearPreferenceActivity extends PreferenceActivity {
 
@@ -16,12 +20,22 @@ public class TheWearPreferenceActivity extends PreferenceActivity {
 				getString(R.string.TheWear_preference_key));
 		getPreferenceManager().setSharedPreferencesMode(MODE_PRIVATE);
 		addPreferencesFromResource(R.xml.preferences);
+		setUpInActionBar();
+		
+	}
+
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	// The new API is only used if the device has API level 11 or higher
+	public void setUpInActionBar() {
+		if (Build.VERSION.SDK_INT > Build.VERSION_CODES.GINGERBREAD_MR1) {
+			ActionBar actionBar = getActionBar();
+		    actionBar.setDisplayHomeAsUpEnabled(true);
+		    Log.i("BUG","Done");
+		}
 	}
 
 	// TODO: Move Forecast Preferences here if we think this won't be changed a
 	// lot
-
-	// ListPreference for the region thingy
 
 	// TODO set to defaults
 }
